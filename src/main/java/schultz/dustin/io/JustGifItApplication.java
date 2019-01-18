9c38b2cbc6e2ad3566b511a2fe3d5c987a223d15
ayoub <aelmoukhtafi@finaxys.com>
2019-01-18 11:45:49 +0100

    custom autoconfiguration

--- src/main/java/schultz/dustin/io/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/JustGifItApplication.java
@@ -1,43 +1,16 @@
 package schultz.dustin.io;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class JustGifItApplication {
 
-    @Value("${multipart.location}/gif/")
-    private String gifLocation;
 
     public static void main(String[] args) {
         SpringApplication.run(JustGifItApplication.class, args);
     }
 
-    @PostConstruct
-    private void init() {
-        File gifFolder = new File(gifLocation);
-        if (!gifFolder.exists()) {
-            gifFolder.mkdir();
-        }
-    }
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/gif/**")
-                        .addResourceLocations("file:" + gifLocation);
-                super.addResourceHandlers(registry);
-            }
-        };
-    }
 }
